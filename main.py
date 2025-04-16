31776d0392b091237115eb0c80339a9e490cad96
Patrik Urban <patrikurban.work@gmail.com>
2025-04-16 22:08:41 +0200

    add try except for init docker client, and start container

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import sys
+import docker
 import urllib3
+from docker.errors import DockerException
 from services.docker_client import DockerClient
 from urllib3.exceptions import InsecureRequestWarning
 
 # Disable SSL warnings globally for urllib3
 urllib3.disable_warnings(InsecureRequestWarning)
-client = DockerClient()
+
+try:
+    client = DockerClient()
+except DockerException as e:
+    print("Docker is not running or cannot be reached. Please start Docker and try again.")
+    sys.exit(1)
+except Exception as e:
+    print(f"An error occurred while initializing DockerClient: {str(e)}")
+    sys.exit(1)
 
 images = client.list_images()
 for image in images:
@@ -14,7 +25,8 @@ data = client.list_containers()
 for container in data:
     print(container)
 
-container_id = input("Enter the container ID to start: ")
-
-client.start_container(container_id)
-# print(f"Container {container_id} started.")
+try:
+    container_id = input("Enter the container ID to start: ")
+    client.start_container(container_id)
+except Exception as e:
+    print(f"Failed to start container '{container_id}': {str(e)}")
